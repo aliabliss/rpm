434c27edc180cd1d48eb548995cb846d8fced62e
aliabliss <aupovaalbina3@gmail.com>
2025-09-11 00:13:06 +0500

    Add files via upload

---
+++ дз2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+struct circle
+{
+	double coordinatesofthecenter1;
+	double coordinatesofthecenter2;
+	double radius;
+	string colour;
+};
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int N;
+	cout << "введи количество окружностей:" << endl;
+	cin >> N;
+
+	cin.ignore();
+	circle* circle1 = new circle[N];
+	for (int i = 0; i < N; i++)
+	{
+		cout << "введи координаты центра: " << i + 1 << " -ой окружности " << endl;
+		cin >> circle1[i].coordinatesofthecenter1;
+		cin >> circle1[i].coordinatesofthecenter2;
+		cout << "введи радиус: " << i + 1 << " -ой окружности" << endl;
+		cin >> circle1[i].radius;
+		cout << "введи цвет: " << i + 1 << " -ой окружности" << endl;
+		cin.ignore();
+		getline(cin, circle1[i].colour);
+
+
+	}
+	cout << endl;
+	cout << "все окружности" << endl;
+	cout << endl;
+	for (int i = 0; i < N; i++)
+	{
+		cout << setw(4) << "окружность: " << i + 1 << " -я " << endl;
+		cout << setw(4) << "центры: " << circle1[i].coordinatesofthecenter1 << ", " << circle1[i].coordinatesofthecenter2 << endl;
+		cout << setw(4) << "радиус: " << circle1[i].radius << endl;;
+		cout << setw(4) << "цвет: " << circle1[i].colour << endl;
+		cout << endl;
+	}
+
+}
---
+++ дз3.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct chemicalelement
+{
+	string title;
+	string symbol;
+	int sequencenumber;
+	double atomicmass;
+};
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int N;
+	cout << "введите количество химических элементов: " << endl;
+	cin >> N;
+	cin.ignore();
+	chemicalelement* element = new chemicalelement[N];
+	for (int i = 0; i < N; i++)
+	{
+		cout << "введи название " << i + 1 << " -го химического элемента: " << endl;
+		getline(cin, element[i].title);
+		cout << "введи символ " << i + 1 << " -го химического элемента: " << endl;
+		getline(cin, element[i].symbol);
+		cout << "введи порядковый номер " << i + 1 << " -го химического элемента: " << endl;
+		cin >> element[i].sequencenumber;
+		cout << "введи атомную массу " << i + 1 << " -го химического элемента: " << endl;
+		cin >> element[i].atomicmass;
+		cin.ignore();
+
+
+	}
+	cout << endl;
+	cout << "все химические элементы" << endl;
+	cout << endl;
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << setw(4) << "химический элемент под номером: " << i + 1 << endl;
+		cout << setw(4) << "название: " << element[i].title << endl;
+		cout << setw(4) << "символ: " << element[i].symbol << endl;;
+		cout << setw(4) << "порядковый номер: " << element[i].sequencenumber << endl;
+		cout << setw(4) << "атомная масса: " << element[i].atomicmass << endl;
+		cout << endl;
+
+
+
+	}
+
+}
